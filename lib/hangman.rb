470cb225d667eea66128b742e52d0611a1d9f8b9
Aaron Collins <aa.collins.95@gmail.com>
2019-04-09 22:45:16 -0400

    Functioning hangman, no saving yet

---
+++ lib/hangman.rb
@@ -0,0 +1,125 @@
+class Game
+
+  def initialize
+    @root = "/home/aaron/TOP/ruby_programming/hangman/"
+    @empty_chr = "_"
+    @word = get_word
+    puts @word
+    @word_array = Array.new(@word.length,@empty_chr)
+    puts @word.length
+    @lives = 5
+    @game_over = false
+    @guesses = []
+    run
+  end
+
+  def run
+    #runs the game
+    until @game_over
+      display_ui
+      check_endgame
+      guess = get_guess if !@game_over
+      update(guess)
+    end
+    display_gameover
+
+  end
+  def load_game
+    #loads game from a file
+    #reassigns word, word_array, lives, empty_chr, and guesses
+    pass
+  end
+
+  def get_word
+    #loads file, chooses a random word between 5 and 12 letters long
+    puts "Generating random word..."
+    words = File.readlines("5desk.txt").select do |line|
+      line.chomp if (5..12).include?(line.chomp.length)
+    end
+    puts "Word generated!"
+    return words[rand(words.length-1)].downcase.chomp
+  end
+
+  def get_guess
+    #ensures guess is valid
+    print "Next guess?"
+    valid = false
+    until valid
+      prelim = gets.chomp.downcase
+      prelim = "$" if prelim == ""
+      is_letter = (97..122).include?(prelim.ord)
+      is_guessed = @guesses.include?(prelim)
+      if is_letter && !is_guessed
+        valid = prelim.length == 1
+      else
+        puts "Please input a letter that you haven't guessed yet!"
+      end
+    end
+    return prelim
+  end
+
+#nil input
+
+  def display_ui
+    #clears screen
+    puts "\n" * 30
+    #Prints stuff
+    draw_hidden_word
+    draw_guessed
+    draw_lives
+  end
+
+  def draw_hidden_word
+    #draws the word in the hidden format
+    puts @word_array.join("   ")
+    puts " "
+  end
+
+  def draw_guessed
+    #draws a list of letters that have been guessed but were wrong
+    print "Incorrect Guesses: "
+    print @guesses.select {|g| !@word.include?(g)} if @lives < 5
+    print "\n"
+  end
+
+  def draw_lives
+    #displays the number of lives left
+    puts "Lives: #{@lives}"
+  end
+
+  def check_endgame
+    #evaluates if player ran out of guesses, or correctly guessed the word
+    dead = (@lives == 0)
+    win = (@word_array.all? {|w| w != @empty_chr})
+    @game_over = true if dead || win
+  end
+
+  def update(guess)
+    #refreshes the independent variables; guesses and word_array
+    @lives = 5
+    @guesses.push(guess)
+    @guesses.each { |g|
+      if @word.include?(g)
+        @word.split("").each_with_index { |c,i| @word_array[i] = g if c == g }
+      else
+        @lives -= 1
+      end
+    }
+  end
+
+  def display_gameover
+    if @lives < 1
+      puts "You Lost"
+      puts "The word was #{@word}"
+    else
+      puts "You won the game!"
+    end
+  end
+
+
+
+
+
+end
+
+Game.new
